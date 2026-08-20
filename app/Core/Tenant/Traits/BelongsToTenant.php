d6a6b065dca28494098d8cdd365e7eff93d3200d
Wahieng <wahidfajarwahyudi@gmail.com>
2026-08-20 09:17:31 +0700

    p0 untuk core

--- app/Core/Tenant/Traits/BelongsToTenant.php
+++ app/Core/Tenant/Traits/BelongsToTenant.php
@@ -2,14 +2,40 @@
 
 namespace App\Core\Tenant\Traits;
 
+use App\Core\Tenant\Context\TenantContext;
 use App\Core\Tenant\Models\Tenant;
+use App\Core\Tenant\Scopes\TenantScope;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 trait BelongsToTenant
 {
     /**
+     * Boot trait.
+     */
+    protected static function bootBelongsToTenant(): void
+    {
+        static::addGlobalScope(
+            app(TenantScope::class)
+        );
+
+        static::creating(function (Model $model): void {
+            $context = app(TenantContext::class);
+
+            /*
+             * Jika tenant context tersedia,
+             * otomatis isi tenant_id.
+             */
+            if ($context->check() && empty($model->tenant_id)) {
+                $model->tenant_id = $context->require();
+            }
+        });
+    }
+
+    /**
      * Relasi model ke tenant.
      */
-    public function tenant()
+    public function tenant(): BelongsTo
     {
         return $this->belongsTo(
             Tenant::class,
